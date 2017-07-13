d41584fb813795a577eb6ef3f1fe747c102d2a14
saarthak <keonemadrid40@gmail.com>
2017-07-13 11:41:50 +0530

    To  get the user info using the user_id

--- user_info.py
+++ user_info.py
@@ -1,23 +1,27 @@
 import requests
-from constants import APP_Access_Token,Base_url
-def get_user_id(user_name):
-
-    request_url = Base_url + 'users/search?q=%s&access_token=%s'%(user_name,APP_Access_Token)
-    info = requests.get(request_url).json()
-
-    if info['meta']['code'] == 200:
-        if len(info['data']):
-                return info['data'][0]['id']
-        else:
-            print( 'none')
-    else:
-        print("Status code other than 200 received")
-        exit()
-
+from user_id import get_user_id
+from constants import *
+#
 def get_user_info(user_name):
-    user_id=get_user_id(user_name)
-    response="{}/users/{}/{}".format(Base_url,user_id,APP_Access_Token)
-    
+    user_id = get_user_id(user_name)
+    if user_id == None :
+        print("No such user")
+        exit()
+    response = "{}users/{}/?access_token={}".format(Base_url,user_id,APP_Access_Token)
+    print response
+    object = requests.get(response).json()
+    if object['meta']['code']==200:
+        if len(object['data']):
+            print(object['data']['username'])
+        else:
+            print('none')
+    else:
+        print('object code other than 200 found')
+
+get_user_info('sharma_abhi07')
+
+
+
 
 
 
