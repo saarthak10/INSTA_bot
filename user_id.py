68cf0db74dd4bfa34de09fd58a362ec1d02e5acd
saarthak <keonemadrid40@gmail.com>
2017-07-18 23:21:32 +0530

    function for accessing user's id

--- user_id.py
+++ user_id.py
@@ -2,7 +2,7 @@ import requests
 from constants import APP_Access_Token,Base_url
 # function to generate user_id
 def get_user_id(user_name):
-
+#writing the url by using our base_url and app_access_token and the endpoint for user's search by name
     request_url = Base_url + 'users/search?q=%s&access_token=%s'%(user_name,APP_Access_Token)
     info = requests.get(request_url).json()
 
