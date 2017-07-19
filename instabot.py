6c0caddf6e2c404d36cd88bcce16932408336b7c
saarthak <keonemadrid40@gmail.com>
2017-07-19 19:51:45 +0530

    some errors resolved

--- instabot.py
+++ instabot.py
@@ -12,37 +12,39 @@ from comments import post_comment
 
 def insta_bot():
 
-    Name= raw_input("Enter your instabot username")
+    Name= raw_input("Enter your instabot username:")
     print("WELCOME TO INSTABOT!!!\n")
     print("Hello %s\n")%(Name)
     print("What would you like to do?\n")
     while True:
         choose = raw_input("1)Get own information\n2)Get user id\n3)Get user's information\n4)Get your own recent post\n5)Get user's recent post\n6)Post a like on a user's post\n"
-                       "7)Comment on a user's post\n8)Get a list of comments on a user's post\n9)Analyse popular trends\n10)Exit")
+                       "7)Comment on a user's post\n8)Get a list of comments on a user's post\n9)Analyse popular trends\n10)Exit\n")
         if choose.isdigit():
             choose = int(choose)
         if choose == 1:
             self_info()
         elif choose == 2:
-            Username = raw_input("Enter username of the user whose id is required")
+            Username = raw_input("Enter username of the user whose id is required:")
             get_user_id(Username)
         elif choose == 3:
-            user_name = "Enter the username of the user whose details are required"
-            get_user_info()
+            user_name = "Enter the username of the user whose details are required:"
+            get_user_info(user_name)
         elif choose == 4:
             self_media()
         elif choose == 5:
-            user_media()
+            user_name = raw_input("Enter the username of the user:")
+            user_media(user_name)
         elif choose == 6:
-            user_name = raw_input("Enter the username of the user")
+            user_name = raw_input("Enter the username of the user:")
             like_post(user_name)
         elif choose == 7:
-            user_name = ("Enter the username of the user ")
+            user_name = ("Enter the username of the user: ")
             post_comment(user_name)
         elif choose == 8:
-            analysing_trends()
+            user_name = raw_input("Enter the username of the user:")
+            get_comments_list(user_name)
         elif choose == 9:
-            user_name = raw_input("Enter the username of the user")
+            analysing_trends()
         elif choose == 10:
             break
         else:
